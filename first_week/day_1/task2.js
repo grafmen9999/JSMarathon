093d4a85e2e6996754176e84573f4bc606ea871a
grafmen <grafmen9999@gmail.com>
2020-11-16 15:29:44 +0200

    homework#1 task2

--- first_week/day_1/task2.js
+++ first_week/day_1/task2.js
@@ -3,34 +3,58 @@ const task2 = () => {
     formattedPhone: (phone) => {
       // ********************************************************************
       // why not? :D
-      // const regExpFormat = '(\\+7)(\\d{3})(\\d{3})(\\d{2})(\\d{2})';
+      // const regExpFormat = '(\\+7)?(\\d{3})(\\d{3})(\\d{2})(\\d{2})';
       // const f = (new RegExp(regExpFormat)).exec(phone);
       //
-      // return `${f[1]} (${f[2]}) ${f[3]}-${f[4]}-${f[5]}`;
+      // return `${f[1] ?? '+7'} (${f[2]}) ${f[3]}-${f[4]}-${f[5]}`;
       //
       // так как требуется именно такой способ, то сделаю такой :c
       // но через регулярки мне нравится больше c:
       // ********************************************************************
       let res = '';
+      let q = 0;
 
-      for (let i = 0; i < phone.length; i++) {
+      const length = phone.length;
+
+      const firstElem = phone.charAt(0);
+      const afterFirstElem = phone.charAt(1);
+
+      if ((firstElem !== '+' && length > 10 && firstElem !== '7' && firstElem !== '8' && firstElem !== '9') || (firstElem === '+' && afterFirstElem !== '7')) {
+        // console.error('Bad phone number!', phone);
+        throw new Error('Bad phone number!');
+      }
+
+      for (let i = 0; i < length; i++) {
+        if (phone.charAt(i) !== '+' && !i) {
+          if (length === 10) {
+            res += '+7'
+            q = 2;
+          } else if (length > 10) {
+            res += '+7'
+            q = 1;
+            i++;
+          }
+        }
+
         switch (i) {
-          case 2: {
+          case 2 - q: {
             res += ` (${phone.charAt(i)}`;
             break;
           }
 
-          case 5: {
+          case 5 - q: {
             res += `) ${phone.charAt(i)}`;
             break;
           }
 
-          case 8: case 10: {
+          case 8 - q:
+          case 10 - q: {
             res += `-${phone.charAt(i)}`;
             break;
           }
 
-          default: res += phone.charAt(i);
+          default:
+            res += phone.charAt(i);
         }
       }
 
